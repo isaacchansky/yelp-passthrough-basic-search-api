525a49aea3636c3cbdb5258484ee6ed470dfe0b9
Isaac Chansky <ichansky@gmail.com>
2016-03-26 00:01:33 -0400

    add cors

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-var env = require('./env');
+var env = require('./env')
 
-var express = require('express');
-var app = express();
-// Request API access: http://www.yelp.com/developers/getting_started/api_access
-var Yelp = require('yelp');
+var express = require('express')
+var app = express()
+var cors = require('cors')
+var Yelp = require('yelp')
+
+app.use(cors());
+
 
 var yelp = new Yelp({
   consumer_key: env.CONSUMER_KEY,
   consumer_secret: env.CONSUMER_SECRET,
   token: env.TOKEN,
   token_secret: env.SECRET_TOKEN,
-});
+})
 
 app.get('/search', function (req, res) {
   console.log(req.query)
   yelp.search({ term: req.query.term, location: req.query.location })
   .then(function (data) {
-    res.json(data);
-    console.log(data);
+    res.json(data)
   })
   .catch(function (err) {
-    res.json(err);
-    console.error(err);
-  });
+    res.json(err)
+  })
 
-});
+})
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+  console.log('Example app listening on port 3000!')
+})
